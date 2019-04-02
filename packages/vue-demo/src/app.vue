9f3595de749b3716d7c73fabe343ccedc26ff948
Chris Jaynes <chris@Chriss-MBP-2.zyxel.com>
2019-04-01 22:14:43 -0500

    add decaySpeed property

--- packages/vue-demo/src/app.vue
+++ packages/vue-demo/src/app.vue
@@ -23,6 +23,15 @@ export default {
       const gui = new dat.GUI({ closed: true })
       gui.add(this.store, "lightSize", { sm: 30, md: 50, lg: 80, xl: 110 })
       gui.add(this.store, "fadeSpeed", { slow: 750, med: 500, fast: 250 })
+      gui.add(this.store, "decaySpeed", {
+        never: 0,
+        "15s": 10000,
+        "10s": 10000,
+        "5s": 5000,
+        "3s": 3000,
+        "2s": 2000,
+        "1s": 1000
+      })
       const colors = gui.addFolder("colors")
       colors.addColor(this.store, "color0")
       colors.addColor(this.store, "color1")
--- packages/vue/src/store/create-store.js
+++ packages/vue/src/store/create-store.js
@@ -7,14 +7,16 @@ const createStore = () => {
       height: 100,
       lightSize: 50,
       fadeSpeed: 500,
-      lights: {},
-      touches: {},
+      decaySpeed: 0,
       color0: "#FF4136",
       color1: "#FF851B",
       color2: "#FFDC00",
       color3: "#2ECC40",
       color4: "#0074D9",
-      color5: "#B10DC9"
+      color5: "#B10DC9",
+      lights: {},
+      touches: {},
+      records: []
     },
     computed: {
       colors () {
@@ -33,6 +35,22 @@ const createStore = () => {
       rowCount () {
         return Math.floor(this.height / this.lightSize) || 0
       }
+    },
+    watch: {
+      decaySpeed (newSpeed, oldSpeed) {
+        if (Number(oldSpeed) === 0 && Number(newSpeed) > 0 && this.records.length > 0) {
+          const now = new Date().valueOf()
+          const offset = this.records[0].time
+          this.records = this.records.map((record) => {
+            const time = (now - offset) + (record.time) + Number(newSpeed)
+            const { key } = record
+            return { time, key }
+          })
+        }
+      }
+    },
+    created () {
+      requestAnimationFrame(this.processDecays)
     },
     methods: {
       getColor (row, col) {
@@ -73,6 +91,7 @@ const createStore = () => {
         if (!this.onTouchList(identifier, key)) {
           this.updateTouchList(identifier, key)
           const colorIndex = this.nextColor(this.lights[key].colorIndex)
+          this.addRecord(key, colorIndex)
           Vue.set(this.lights, key, { colorIndex })
         }
       },
@@ -87,6 +106,37 @@ const createStore = () => {
           return 0
         }
         return colorIndex + 1
+      },
+      previousColor (colorIndex) {
+        if (colorIndex > 0) {
+          return colorIndex - 1
+        }
+        return 0
+      },
+      addRecord (key) {
+        const time = new Date().valueOf()
+        this.records.push({ time, key })
+      },
+      processDecays () {
+        if (this.decaySpeed > 0) {
+          const now = new Date().valueOf()
+          for (var i = 0; i < this.records.length; i++) {
+            const { time, key } = this.records[i]
+            if (now - time > this.decaySpeed) {
+              setTimeout(() => {
+                this.processDecay(key)
+              }, 0)
+            } else {
+              break;
+            }
+          }
+          this.records.splice(0, i)
+        }
+        requestAnimationFrame(this.processDecays)
+      },
+      processDecay (key) {
+        const { colorIndex } = this.lights[key]
+        this.lights[key].colorIndex = this.previousColor(colorIndex)
       },
       resize ({ width, height }) {
         this.width = width
